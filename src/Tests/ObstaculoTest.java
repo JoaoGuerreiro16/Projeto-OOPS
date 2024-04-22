7a6107f12de9a4b5d4d65dcc9ce2fb7f3dd9fabf
Light1800 <167210004+Light1800@users.noreply.github.com>
2024-04-22 19:50:06 +0100

    adad

---
+++ src/Tests/ObstaculoTest.java
@@ -0,0 +1,11 @@
+package Tests;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+
+public class ObstaculoTest {
+}
