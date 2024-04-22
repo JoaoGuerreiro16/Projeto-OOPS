84c87b1bfba5bf1ca622eea2192e6c03e59afc5b
Light1800 <167210004+Light1800@users.noreply.github.com>
2024-04-22 22:26:46 +0100

    construtor classe abstrata
    fiz o construtor para receber o centro porque é uma cena que ambas as comidas tem em comum

--- src/Game/Comida.java
+++ src/Game/Comida.java
@@ -2,4 +2,13 @@ package Game;
 
 public abstract class Comida {
 
+    protected int xCentro;
+    protected int yCentro;
+
+    public Comida(int x, int y){
+
+        this.xCentro = x;
+        this.yCentro = y;
+    }
+
 }
